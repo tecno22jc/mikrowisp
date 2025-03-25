0525699269d682e91806eeecddfdffdb5c9ee7fc
tecno22jc <carcasi.87gmail.com>
2025-03-25 15:05:06 -0500

    Configurado puerto para Railway

--- app.py
+++ app.py
@@ -65,4 +65,6 @@ def delete_client(id):
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port) 
